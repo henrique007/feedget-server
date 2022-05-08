c686a48a40f143e29f5513e966ddcf5f7ffb5c6b
henrique007 <dayvsonn.ti@outlook.com>
2022-05-07 23:28:57 -0300

    configurando o envio da foto para o email

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -37,11 +37,14 @@ export class SubmitFeedbackUseCase {
     })
   
     await this.mailAdapter.sendMail({
-      subject: 'Novo Feedback',
-      body:  [
-        `<p>Tipo de feedback: ${type}</p>`,
-        `<p>Comentário: ${comment}</p>`,
-    ].join('\n')
-    })
+      subject: 'Novo feedback!',
+      body: [
+          `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+          `<p>Tipo de Feedback: ${type}</p>`,
+          `<p>Comentário: ${comment}</p>`,
+          screenshot ? `<img src="${screenshot}" />` : ``,
+          `</div>`
+          ].join('\n')
+  })
   }
 }
